71bfcc2e0880c46334222dc2cbe693e4ef91d33b
Meetali <meetalideoke@gmail.com>
2023-09-25 13:37:52 +0530

    auth

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: '166868863171-3jc87rbv266kcefu4f2jqjlhsqrbfm1p.apps.googleusercontent.com',
     clientSecret: "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",
-    callbackURL: "https://frontend-x0qa.onrender.com/auth/google/callback"
+    callbackURL: "https://blogapp-api-lxve.onrender.com/auth/google/callback"
   },
   (accessToken, refreshToken, profile, done) => {
    
